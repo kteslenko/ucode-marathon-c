5b28221261eba4b9f46618be6e7a7e35258e3029
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-06 16:04:40 +0300

    sprint01: Completed task 08

--- sprint01/t02/mx_write_knock_knock.c
+++ sprint01/t02/mx_write_knock_knock.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 
 void mx_write_knock_knock(void) {
-    const char* text = "Follow the white rabbit.\nKnock, knock, Neo.\n";
+    const char *text = "Follow the white rabbit.\nKnock, knock, Neo.\n";
+
     write(1, text, strlen(text));
 }
