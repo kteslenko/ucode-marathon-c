f6b3e901428af9da48e0016600be0dd94736c3c7
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-07 16:29:57 +0300

    sprint02: Minor fixes t13

--- sprint02/t13/mx_sum_digits.c
+++ sprint02/t13/mx_sum_digits.c
@@ -1,12 +1,13 @@
 int mx_sum_digits(int num) {
     int sum = 0;
 
-    if (num < 0) {
-        num *= -1;
-    }
+    while (num != 0) {
+        int digit = num % 10;
 
-    while (num > 0) {
-        sum += num % 10;
+        if (digit < 0) {
+            digit *= -1;
+        }
+        sum += digit;
         num /= 10;
     }
 
