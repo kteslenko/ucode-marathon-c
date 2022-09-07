fc5ebb0943b1ad3c8af1b3ffbb2192c8bdc3bfcb
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-07 10:41:11 +0300

    sprint02: Completed task 07

--- sprint02/t00/mx_is_positive.c
+++ sprint02/t00/mx_is_positive.c
@@ -3,9 +3,11 @@ void mx_printstr(const char *s);
 void mx_is_positive(int i) {
     if (i > 0) {
         mx_printstr("positive\n");
-    } else if (i < 0) {
+    }
+    else if (i < 0) {
         mx_printstr("negative\n");
-    } else {
+    }
+    else {
         mx_printstr("zero\n");
     }
 }
