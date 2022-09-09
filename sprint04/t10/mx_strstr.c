ebd946eb9a5c4d2a9e4476ea3f5f977db138751f
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-09 15:50:33 +0300

    sprint04: Completed task 10

---
+++ sprint04/t10/mx_strstr.c
@@ -0,0 +1,16 @@
+int mx_strlen(const char *s);
+char *mx_strchr(const char *s, int c);
+int mx_strncmp(const char *s1, const char *s2, int n);
+
+char *mx_strstr(const char *s1, const char *s2) {
+    int s2_len = mx_strlen(s2);
+
+    while (s1 != 0) {
+        if (mx_strncmp(s1, s2, s2_len) == 0) {
+            return (char*)s1;
+        }
+        s1 = mx_strchr(s1 + 1, *s2);
+    }
+
+    return 0;
+}
