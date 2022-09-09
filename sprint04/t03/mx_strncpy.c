b34ebfe47ab68eb6e420e8fef68c926ec7614675
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-09 11:32:55 +0300

    sprint04: Completed task 03

---
+++ sprint04/t03/mx_strncpy.c
@@ -0,0 +1,20 @@
+char *mx_strncpy(char *dst, const char *src, int len) {
+    int i;
+
+    for (i = 0; i < len
+         && src[i] != '\0'; i++) {
+        dst[i] = src[i];
+    }
+
+    while (i < len) {
+        dst[i] = '\0';
+        i++;
+    }
+
+    return dst;
+}
+
+
+int main() {
+
+}
