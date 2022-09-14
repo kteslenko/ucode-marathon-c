3e5f8a55502470db8098c3c854e45f868c545d83
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-14 16:00:16 +0300

    sprint06: Changed task 04

--- sprint06/t04/mx_insertion_sort.c
+++ sprint06/t04/mx_insertion_sort.c
@@ -1,22 +1,20 @@
 int mx_strlen(const char *s);
 
-static void swap(char** a, char** b) {
-    char* temp = *a;
-
-    *a = *b;
-    *b = temp;
-}
-
 int mx_insertion_sort(char **arr, int size) {
-    int swaps = 0;
+    int shifts = 0;
 
     for (int i = 1; i < size; i++) {
-        for (int j = i; j > 0
-             && mx_strlen(arr[j - 1]) > mx_strlen(arr[j]); j--) {
-            swap(&arr[j - 1], &arr[j]);
-            swaps++;
+        char *key = arr[i];
+        int j;
+
+        for (j = i - 1; j >= 0
+             && mx_strlen(arr[j]) > mx_strlen(key); j--) {
+            arr[j + 1] = arr[j];
+            shifts++;
         }
+
+        arr[j + 1] = key;
     }
 
-    return swaps;
+    return shifts;
 }
