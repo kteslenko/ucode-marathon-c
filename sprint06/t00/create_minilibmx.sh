9d2ec1bf31c913789efc25fbaf47afd0bade3876
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-14 13:32:39 +0300

    sprint06: Fixed task 00

--- sprint06/t00/create_minilibmx.sh
+++ sprint06/t00/create_minilibmx.sh
@@ -1,5 +1,5 @@
 files=("mx_printchar.c" "mx_printint.c" "mx_printstr.c" "mx_strcpy.c" "mx_strlen.c" "mx_strcmp.c" "mx_isdigit.c" "mx_isspace.c" "mx_atoi.c")
-objects=${files[@]/.c/.o}
+objects=("mx_printchar.o" "mx_printint.o" "mx_printstr.o" "mx_strcpy.o" "mx_strlen.o" "mx_strcmp.o" "mx_isdigit.o" "mx_isspace.o" "mx_atoi.o")
 clang -c -std=c11 -Wall -Wextra -Werror -Wpedantic ${files[@]}
 ar rsc minilibmx.a ${objects[@]}
 rm ${objects[@]}
