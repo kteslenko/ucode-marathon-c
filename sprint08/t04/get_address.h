aff397e55aaa02cec43c6b4c821d03a451fd2628
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-16 14:21:25 +0300

    sprint08: Completed task 04

---
+++ sprint08/t04/get_address.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int mx_strlen(const char *s);
+char *mx_get_address(void *p);
+char *mx_strnew(const int size);
+char *mx_nbr_to_hex(unsigned long nbr);
+char *mx_strcpy(char *dst, const char *src);
