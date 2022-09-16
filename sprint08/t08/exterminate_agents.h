d54557aa717a545a8335247279cb8797ed45b88c
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-16 18:37:45 +0300

    sprint08: Completed task 08

---
+++ sprint08/t08/exterminate_agents.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "agent.h"
+
+#include <stdlib.h>
+
+void mx_exterminate_agents(t_agent ***agents);
---
+++ sprint08/t08/mx_exterminate_agents.c
@@ -0,0 +1,15 @@
+#include "exterminate_agents.h"
+
+void mx_exterminate_agents(t_agent ***agents) {
+    if (agents == NULL) {
+        return;
+    }
+    if (*agents == NULL) {
+        return;
+    }
+    for (int i = 0; (*agents)[i] != NULL; i++) {
+        free((*agents)[i]);
+    }
+    free(*agents);
+    *agents = NULL;
+}
