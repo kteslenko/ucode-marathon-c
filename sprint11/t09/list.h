b1d37df6127c9418554d4e62595d054d0472214a
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-22 15:55:43 +0300

    sprint11: Removed header 09

--- sprint11/t09/list.h
+++ sprint11/t09/list.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <stdlib.h>
-
 typedef struct s_list {
     void *data;
     struct s_list *next;
--- sprint11/t09/mx_foreach_list.c
+++ sprint11/t09/mx_foreach_list.c
@@ -1,7 +1,7 @@
 #include "list.h"
 
 void mx_foreach_list(t_list *list, void (*f)(t_list *node)) {
-    while (list != NULL) {
+    while (list != 0) {
         f(list);
         list = list->next;
     }
