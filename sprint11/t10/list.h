14dbed9bb6e7bc05a84b34a84f981517cbfee8e3
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-22 15:55:55 +0300

    sprint11: Completed task t10

---
+++ sprint11/t10/list.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef struct s_list {
+    void *data;
+    struct s_list *next;
+}              t_list;
+
+
+t_list *mx_sort_list(t_list *list, bool (*cmp)(void *a, void *b));
